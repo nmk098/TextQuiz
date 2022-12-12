9426cbf75c538c99f76d3e628ed3964e289a2a0f
nmk098 <nkuryumov@gmail.com>
2022-12-12 13:02:27 +0400

    Initial Commit

---
+++ TextQuiz/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TextQuiz
+//
+//  Created by Никита Курюмов on 12.12.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
