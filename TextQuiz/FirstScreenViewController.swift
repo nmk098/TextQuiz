89452f80eafd6b5c734e6fcc6f3a74c9ecb22d98
nmk098 <nkuryumov@gmail.com>
2022-12-12 14:30:38 +0400

    Add first settings for view's elements:
    - goButton from mainScreen
    - textField for typing your name

---
+++ TextQuiz/FirstScreenViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstScreenViewController.swift
+//  TextQuiz
+//
+//  Created by Никита Курюмов on 12.12.22.
+//
+
+import UIKit
+
+class FirstScreenViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .darkGray
+        
+    }
+    
+
+   
+}
